d1ca4c6be850b6929610859342a543153a8e945c
EldarSarajlic <eldarsarajlic525@gmail.com>
2025-02-26 23:23:01 +0100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Build.Framework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Persistence;
@@ -12,12 +13,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseAuthorization();
-
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:3000","https://localhost:3000"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
